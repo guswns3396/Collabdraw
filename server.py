8ae9f928fcf9ce4114196bbf89b28f1e79c58e86
Hyun-Joon Yang <yhyunjoon@gmail.com>
2020-06-10 12:26:37 -0700

    initial server setup

--- server.py
+++ server.py
@@ -3,4 +3,16 @@ from flask_socketio import SocketIO
 
 # create Flask object
 app = Flask(__name__)
-app
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
+
+@socketio.on('connect')
+def test_connection():
+    print("connected")
+
+@socketio.on('stroke')
+def handle_stroke(json):
+    print("received json: " + str(json))
