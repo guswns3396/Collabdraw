ba05d7f356de52ad3013f047b8d628025c1b9b0d
Hyun-Joon Yang <yhyunjoon@gmail.com>
2020-06-19 15:28:46 -0700

    added newline at end of server.py

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_socketio import SocketIO, emit
+from flask_cors import CORS
 
 # create Flask object
 app = Flask(__name__)
+CORS(app)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
