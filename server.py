a641a2acadccee4f01df280b8abf2895b5c5d308
Hyun-Joon Yang <yhyunjoon@gmail.com>
2020-06-19 19:02:31 -0700

    created server class for board state

--- server.py
+++ server.py
@@ -1,14 +1,31 @@
 from flask import Flask
 from flask_socketio import SocketIO, emit
 
+# constants
+PORT = 8080
+HEIGHT = 690
+WIDTH = 1280
+
 # create Flask object
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins='*')
 
+# server class
+class Server:
+    def __init__(self, height, width):
+        self.board = []
+        for i in range(0,height):
+            row = []
+            for j in range(0,width):
+                row.append(0)
+            self.board.append(row)
+server = Server(HEIGHT,WIDTH)
+
 @socketio.on('connect')
 def on_connect():
     print("connected to websocket")
+    emit('receive-update', server.board)
 
 @socketio.on('disconnect')
 def on_disconnect():
@@ -16,8 +33,13 @@ def on_disconnect():
 
 @socketio.on('send-stroke')
 def handle_stroke(json):
+    print(type(json))
     print("received json: " + str(json))
-    emit('receive-update', "myBoard")
+    for pixel in json:
+        x = pixel[0]
+        y = pixel[1]
+        server.board[y][x] = 1
+    emit('receive-update', server.board)
 
 if __name__ == "__main__":
-    socketio.run(app, port=8080, debug=True)
+    socketio.run(app, port=PORT, debug=True)
