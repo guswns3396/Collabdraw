7fbf64dc148e90482a7ce62a3a83de2771732bde
Hyun-Joon Yang <yhyunjoon@gmail.com>
2020-06-19 17:19:57 -0700

    fixed cross-origin resource sharing issue

--- server.py
+++ server.py
@@ -1,12 +1,10 @@
 from flask import Flask
 from flask_socketio import SocketIO, emit
-from flask_cors import CORS
 
 # create Flask object
 app = Flask(__name__)
-CORS(app)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 @socketio.on('connect')
 def on_connect():
