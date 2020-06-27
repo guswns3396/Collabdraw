ae32332b5019bee0913569271dace2990f13089f
Hyun-Joon Yang <yhyunjoon@gmail.com>
2020-06-26 19:14:46 -0700

    renamed serverTest.py to test_server.py

--- test_server.py
+++ test_server.py
@@ -1,7 +1,6 @@
 import unittest
 from unittest.mock import patch
 import io
-import sys
 import server
 
 class TestSocketIO(unittest.TestCase):
@@ -22,21 +21,6 @@ class TestSocketIO(unittest.TestCase):
             client.disconnect()
 
             self.assertEqual(myOutput.getvalue(), expectedOutput)
-
-    def test_if_clients_have_different_ids(self):
-        client1 = server.socketio.test_client(server.app)
-        client2 = server.socketio.test_client(server.app)
-
-        self.assertNotEqual(client1.sid, client2.sid)
-
-    def test_stroke_communication_between_server_and_client(self):
-        client = server.socketio.test_client(server.app)
-
-        client.emit('send-stroke', 'myStroke')
-        received_data = client.get_received()
-        myData = received_data[0]['args'][0]
-
-        self.assertEqual(myData, "myBoard")
 
 if __name__ == '__main__':
     unittest.main()
