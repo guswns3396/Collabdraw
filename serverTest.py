14135779d0f6d74b4bc5f1dfdb1d00cf4bfa4166
Hyun-Joon Yang <yhyunjoon@gmail.com>
2020-06-26 18:19:10 -0700

    commit before merge conflict

--- serverTest.py
+++ serverTest.py
@@ -1,36 +1,41 @@
 import unittest
+from unittest.mock import patch
+import io
+import sys
 import server
 
 class TestSocketIO(unittest.TestCase):
-    def test_if_connection_successful(self):
-        # arrange & act
+    def test_connect_emitsBoardState(self):
         client = server.socketio.test_client(server.app)
-        # assert
-        self.assertTrue(client.is_connected())
+        HEIGHT = 500
+        WIDTH = 500
 
-    def test_if_disconnection_successful(self):
-        # arrange
+        received_data = client.get_received()
+
+        self.assertEqual(received_data[0]['args'][0],[[0]*WIDTH]*HEIGHT)
+
+    def test_disconnect_printsDisconnectionMessage(self):
         client = server.socketio.test_client(server.app)
-        # act
-        client.disconnect()
-        # assert
-        self.assertFalse(client.is_connected())
+        expectedOutput = "disconnected from websocket\n"
+
+        with patch('sys.stdout', new=io.StringIO()) as myOutput:
+            client.disconnect()
+
+            self.assertEqual(myOutput.getvalue(), expectedOutput)
 
     def test_if_clients_have_different_ids(self):
-        # arrange & act
         client1 = server.socketio.test_client(server.app)
         client2 = server.socketio.test_client(server.app)
-        # assert
+
         self.assertNotEqual(client1.sid, client2.sid)
 
     def test_stroke_communication_between_server_and_client(self):
-        # arrange
         client = server.socketio.test_client(server.app)
-        # act
+
         client.emit('send-stroke', 'myStroke')
         received_data = client.get_received()
         myData = received_data[0]['args'][0]
-        # assert
+
         self.assertEqual(myData, "myBoard")
 
 if __name__ == '__main__':
