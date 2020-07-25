8e1d19f554335a3157603ad00ae9b69f06f64eb1
Hyun-Joon Yang <yhyunjoon@gmail.com>
2020-07-24 20:48:37 -0700

    abstracted updateBoard in Server class to updateBoard in CanvasBoard class

--- server.py
+++ server.py
@@ -18,10 +18,10 @@ class Server:
     def __init__(self, imagedata):
         self.board = CanvasBoard(imagedata)
         self.lock = threading.Lock()
-    def updateBoard(self, diff_as_dict):
-        numPixels = len(diff_as_dict['coord'])
-        for i in range(numPixels):
-            self.board.data[diff_as_dict['coord'][i]] = diff_as_dict['val'][i]
+    def updateBoard(self, diff: dict):
+        self.lock.acquire()
+        self.board.updateBoard(diff['coord'], diff['val'])
+        self.lock.release()
 # instantiate server class for board state
 imagedata = {
     'width': WIDTH,
