d828ea22fdca24edcc4511f4205d9da5b48b5e94
Hyun-Bum Yang <yhyunbum@gmail.com>
2020-08-21 22:08:31 -0700

    Create a drawable canvas using React.

--- canvas-react/src/App.js
+++ canvas-react/src/App.js
@@ -1,14 +1,9 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './App.css';
+import Canvas from './Canvas';
 
 function App() {
-  return (
-    <canvas
-      width={window.innerWidth}
-      height={window.innerHeight}
-      onClick={event => alert(event.clientX)}
-    />
-  );
+  return Canvas();
 }
 
 export default App;
---
+++ canvas-react/src/Canvas.js
@@ -0,0 +1,64 @@
+import React, { useRef, useState } from 'react';
+
+function Canvas() {
+  const [isPainting, setIsPainting] = useState(false);
+  const [boardSnapshot, setBoardSnapshot] = useState(null);
+  const canvasRef = useRef(null);
+ 
+  return (
+    <canvas
+      ref={canvasRef}
+      width={window.innerWidth}
+      height={window.innerHeight}
+      onMouseUp={
+        e => {
+          setIsPainting(false);
+          
+          const canvas = canvasRef.current;
+          const ctx = getContext(canvas);
+
+          ctx.beginPath();
+          const boardAfter = getCurrentBoard(canvas);
+          console.log(boardAfter);
+          // Send the board diff over Socket.IO
+        }
+      }
+      onMouseDown={
+        e => {
+          const canvas = canvasRef.current;
+          setBoardSnapshot(getCurrentBoard(canvas));
+          setIsPainting(true);
+          draw(e, canvas);
+        }
+      }
+      onMouseMove={
+        e => {
+          if (isPainting) {
+            draw(e, canvasRef.current);
+          }
+        }
+      }
+    />
+  );
+}
+
+function getContext(canvas) {
+  return canvas.getContext('2d');
+}
+
+function getCurrentBoard(canvas) {
+  return getContext(canvas).getImageData(0, 0, canvas.width, canvas.height);
+}
+
+function draw(e, isPainting, canvas) {
+  const ctx = getContext(canvas);
+
+	ctx.lineWidth = 10;
+	ctx.lineCap = "round";
+	ctx.lineTo(e.clientX, e.clientY);
+	ctx.stroke();
+	ctx.beginPath();
+	ctx.moveTo(e.clientX, e.clientY)
+}
+
+export default Canvas;
