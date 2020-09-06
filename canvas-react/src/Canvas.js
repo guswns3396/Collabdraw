5926e62df53d3860f96322d296f4ef62e3e58f94
Hyun-Bum Yang <yhyunbum@gmail.com>
2020-09-06 00:21:55 -0700

    Migrate frontend to ReactJS (#25)
    * Initial commit with react files.
    
    * Initial canvas creation.
    
    * Create a drawable canvas using React.
    
    * Fix draw method to not include 'isPainting' arg.
    
    * Modify internal states to use useRef instead of useState to prevent re-renders.
    
    * Set up appropriate API calls for sending strokes to backend.
    
    * Add a Dockerfile for canvas react.
    
    * Remove the old frontend files.

---
+++ canvas-react/src/Canvas.js
@@ -0,0 +1,112 @@
+import React, { useRef, useState, useLayoutEffect } from 'react';
+import socketIOClient from "socket.io-client";
+
+const socket = socketIOClient("http://127.0.0.1:8080/canvas");
+
+function Canvas() {
+  const isPainting = useRef(false);
+  const boardSnapshot = useRef(null);
+  const canvasRef = useRef(null);
+  
+  const [canvasWidth, setCanvasWidth] = useState(100);
+  const [canvasHeight, setCanvasHeight] = useState(100);
+
+  useLayoutEffect(() => {
+    socket.on("initialize-board", board => {
+      const initialBoard = JSON.parse(board);
+      const initialImagedata = new ImageData(
+        new Uint8ClampedArray(initialBoard.data),
+        initialBoard.width,
+        initialBoard.height
+      );
+
+      setCanvasWidth(initialBoard.width);
+      setCanvasHeight(initialBoard.height);
+      getContext(canvasRef.current).putImageData(initialImagedata, 0, 0);
+    });
+
+    socket.on("broadcast-stroke", stroke => {
+      updateBoard(canvasRef.current, stroke.diffs);
+    });
+  });
+
+  return (
+    <canvas
+      style={{border: "1px solid black"}}
+      ref={canvasRef}
+      width={canvasWidth}
+      height={canvasHeight}
+      onMouseUp={
+        e => {
+          isPainting.current = false;
+
+          const canvas = canvasRef.current;
+          const ctx = getContext(canvas);
+
+          ctx.beginPath();
+          const boardAfter = getCurrentBoard(canvas);
+          console.log(boardAfter);
+          socket.emit("send-stroke", { diffs: getDiffs(boardSnapshot.current, boardAfter) });
+        }
+      }
+      onMouseDown={
+        e => {
+          const canvas = canvasRef.current;
+          boardSnapshot.current = getCurrentBoard(canvas);
+          isPainting.current = true;
+          draw(e, canvas);
+        }
+      }
+      onMouseMove={
+        e => {
+          if (isPainting.current) {
+            draw(e, canvasRef.current);
+          }
+        }
+      }
+    />
+  );
+}
+
+function getContext(canvas) {
+  return canvas.getContext('2d');
+}
+
+function getCurrentBoard(canvas) {
+  return getContext(canvas).getImageData(0, 0, canvas.width, canvas.height);
+}
+
+function draw(e, canvas) {
+  const ctx = getContext(canvas);
+
+  ctx.lineWidth = 10;
+  ctx.lineCap = "round";
+  ctx.lineTo(e.clientX, e.clientY);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(e.clientX, e.clientY)
+}
+
+function updateBoard(canvas, diffs) {
+  const ctx = getContext(canvas);
+  const newBoard = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < diffs.length; i++) {
+    const diff = diffs[i];
+    newBoard.data[diff.coord] = diff.val;
+  }
+  ctx.putImageData(newBoard, 0, 0);
+}
+
+// get difference btw initial & after board
+function getDiffs(boardInitial, boardAfter) {
+  const diffs = [];
+  for (let i = 0; i < boardAfter.data.length; i++) {
+    if (boardInitial.data[i] !== boardAfter.data[i]) {
+      diffs.push({ "coord": i, "val": boardAfter.data[i] });
+    }
+  }
+  console.log(diffs);
+  return diffs;
+}
+
+export default Canvas;
