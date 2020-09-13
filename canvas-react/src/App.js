271e3cca82b2f2d13cb64b306995fc870d1efef9
Hyun-Bum Yang <yhyunbum@gmail.com>
2020-09-13 00:09:25 -0700

    Support rooms.

--- canvas-react/src/App.js
+++ canvas-react/src/App.js
@@ -6,13 +6,17 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import RoomPicker from './RoomPicker';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/">
+        <Route path="/join/:roomId">
           <Canvas />
+        </Route>
+        <Route path="/">
+          <RoomPicker />
         </Route>
       </Switch>
     </Router>
