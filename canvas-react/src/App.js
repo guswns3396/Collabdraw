bd9e775809d35266afd51fa94096e291d037f3aa
Hyun-Bum Yang <yhyunbum@gmail.com>
2020-09-12 23:15:39 -0700

    Set up initial route.

--- canvas-react/src/App.js
+++ canvas-react/src/App.js
@@ -1,8 +1,22 @@
+import React from 'react';
 import './App.css';
 import Canvas from './Canvas';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
-  return Canvas();
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Canvas />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
